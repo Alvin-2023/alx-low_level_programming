37509540f32abba4a7575d16ee32b317e9b83dff
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-19 09:01:09 -0800

    number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,18 @@
 /**
- * Main - entry point of our program
- * @this_that stores our char characters for the string
- * @Puts - prints the string to the output
- * Return - terminate function main
+ *@Main - entry point of our program
+ *@this_that stores our char characters for the string
+ *@Puts - prints the string to the output
+ *@Return - terminate function main
 */
 #include <stdio.h>
 
 int main(void)
 {
-  char this_that[] = "Programming is like building a multilingual puzzle";
+char this_that[] = "Programming is like building a multilingual puzzle";
 
-  puts(this_that);
+puts(this_that);
 
-  putchar('\n');
+putchar('\n');
   
-  return (0);
+return (0);
 } 
