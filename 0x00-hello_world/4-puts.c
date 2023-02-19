5f7b9529f28d4d31073e29e73106d33aea6ef47c
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-19 08:44:21 -0800

    mumber 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+ * Main - entry point of our program
+ * Return - terminate function main
+*/
+#include <stdio.h>
+
+int main(void)
+{
+  char this_that[] = "Programming is like building a multilingual puzzle \n";
+
+  puts(this_that);
+
+  return (0);
+} 
