eb0d3db5f647b250fb639d43722258aed64243bd
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-19 09:53:26 -0800

    number 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-  printf("Size of a char: %lu \n", sizeof(char));
-  printf("Size of an int: %lu \n", sizeof(int));
-  printf("Size of a long: %lu \n", sizeof(long));
-  printf("Size of a long long: %lu \n", sizeof(long long));
-  printf("Size of a float: %lu \n", sizeof(float));
+  printf("Size of a char: %zu \n", sizeof(char));
+  printf("Size of an int: %zu \n", sizeof(int));
+  printf("Size of a long: %zu \n", sizeof(long));
+  printf("Size of a long long: %zu \n", sizeof(long long));
+  printf("Size of a float: %zu \n", sizeof(float));
 
   return (0);
 }
