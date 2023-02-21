794fa6be4c7baedc27e317175030eb855894f25d
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-21 23:35:35 +0000

    number 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,10 @@
 /**
  * main - entry point of our program
+ * print_alphabet_x10 - prints 10 lines of the alphabet in lowercase
  * return - terminates main
  */
 #include <stdio.h>
+/* print_alphabet_x10 - prints 10 lines of the alphabet in lowercase */
 void print_alphabet_x10(void)
 {
 	int i, a;
