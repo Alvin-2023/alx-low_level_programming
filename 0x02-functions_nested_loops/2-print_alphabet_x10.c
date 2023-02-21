2d53c9db3e482a0dc56f038bea8597866c0a57ce
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-21 23:25:38 +0000

    number 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,10 +3,13 @@
  * return - terminates main
  */
 #include <stdio.h>
-void print_alphabet_x10() {
+void print_alphabet_x10(void)
+{
 	int i, a;
-	for (i = 0; i < 10; i++) {
-		for (a = 97; a <= 122; a++) {
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 97; a <= 122; a++)
+		{
 			putchar(a);
 		}
 		putchar('\n');
