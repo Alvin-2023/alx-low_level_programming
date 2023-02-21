077bfa1e327c3c49a622e360b2a9f6e5757fab5f
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-21 23:57:34 +0000

    number 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,10 +7,14 @@
 /*_islower tests to see if a character is upper or lower case */
 int _islower(int c)
 {
-	char c;
+	char ch = 'c';
 
-	if (isLower(c))
+	if (islower(that))
+	{
 		return 1;
+	}
 	else
+	{
 		return 0;
+	}
 }
