697d291974557885a7acead82ca54475ff8cf319
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-21 23:49:43 +0000

    number 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,8 @@
  * main-entry point of our program
  * return - terminates main function
  */
+#include <stdio.h>
+#include <ctype.h>
 /*_islower tests to see if a character is upper or lower case */
 int _islower(int c)
 {
