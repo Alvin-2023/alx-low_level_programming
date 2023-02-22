7951769a69a406d01ace43cde5acd3d92aec357a
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-22 04:57:26 +0000

    number 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,39 +1,17 @@
-/**
- * main - entry poin of our program
- * return - termnates main function
- */
-#include "main.h"
-#include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 
-int print_last_digit(int n)
+int print_last_digit(int num)
 {
-	int lst = n % 10;
+	int last_digit;
+	char digit_char;
 
-	if (lst > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lst);
-	}
-	else if (lst == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, lst);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lst);
-	}
-	return (0);
-}
+	if (num < 0)
+		num = -num;
 
+	last_digit = num % 10;
+	digit_char = last_digit + '0';
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+	write(1, &digit_char, 1);
+
+	return (last_digit);
 }
