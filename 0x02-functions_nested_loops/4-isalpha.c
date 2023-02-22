0ed1afbf956a1ff7e9969860dfba76680e037a43
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-22 00:10:39 +0000

    number 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+/**
+ * main - entry point of our program
+ * return - terminates main function
+ */
+#include <stdio.h>
+#include <cytpe.h>
+
+int _isalpha(int c)
+{
+	int that = 'c';
+
+	if (that = 67)
+		return 1;
+	else
+		return 0;
+}
