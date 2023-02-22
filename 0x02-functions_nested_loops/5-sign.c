dbce41388f0d6f4a4b16e3b57d91c18f4d4f244d
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-22 00:28:04 +0000

    number 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+ * main - entry point of our program
+ * return - terminates main
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int print_sign(int n)
+{
+	int n;
+	if (n > 0)
+	{
+		return +;
+	}
+	if else
+	{
+		return 0;
+	}
+	else
+	{
+		return -;
+	}
+	return (0);
+}
