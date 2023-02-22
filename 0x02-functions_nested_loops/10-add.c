4871d3f9cd7927bb10cf2ab11f3e1052d1db904c
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-22 04:19:39 +0000

    number 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,30 +4,33 @@
  */
 #include "main.h"
 #include <stdio.h>
-
 #include <stdio.h>
 
-int add(int a, int b) {
+int add(int a, int b)
+{
 	int sum = a + b;
 	char buffer[50];
 	int i = 0;
 
 	/* convert the sum to a string */
-	if (sum == 0) {
+	if (sum == 0)
+	{
 		_putchar('0');
 		return 0;
 	}
-	if (sum < 0) {
+	if (sum < 0)
+	{
 		_putchar('-');
 		sum = -sum;
 	}
-	while (sum != 0) {
+	while (sum != 0)
+	{
 		buffer[i++] = (sum % 10) + '0';
 		sum /= 10;
 	}
-
 	/* print the sum in reverse order */
-	while (i > 0) {
+	while (i > 0)
+	{
 		_putchar(buffer[--i]);
 	}
 
