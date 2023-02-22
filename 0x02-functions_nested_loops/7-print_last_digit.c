e3f5344d2539046aee5731f07ee6522187d6b696
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-22 03:27:19 +0000

    number 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+/**
+ * main - entry poin of our program
+ * return - termnates main function
+ */
+#include <stdio.h>
+
+int print_last_digit(int)
+{
+	lst = n % 10;
+
+	if (lst > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lst);
+	}
+	else if (lst == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lst);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lst);
+	}
+	return (0);
+}
