2bd54305fcc83797788aeabfc90c4326a4599aa0
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-24 00:12:31 +0000

    number 0

---
+++ 0x04-more_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ * _isupper - tests wether a character is upper or lowercase
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	int isupper(int c) {
+		return (c >= 'A' && c <= 'Z');
+	}
+}
--- 0x04-more_nested_loops/main.h
+++ 0x04-more_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <ctype.h>
 #include <stdio.h>
 
 int _isupper(int c);
@@ -22,6 +23,32 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void print_triangle(int size);
+
+int isalnum(int c);
+
+int isalpha(int c);
+
+int iscntrl(int c);
+
+int isdigit(int c);
+
+int isgraph(int c);
+
+int islower(int c);
+
+int isprint(int c);
+
+int ispunct(int c);
+
+int isspace(int c);
+
+int isupper(int c);
+
+int isxdigit(int c);
+
+int isascii(int c);
+
+int isblank(int c);
 
 
 #endif /* MAIN_H */
