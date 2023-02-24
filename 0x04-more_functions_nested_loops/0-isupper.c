09e496d3273bdeb8c4f55351593af1279d598a4e
Alvin-2023 <alvintayedzerwa@gmail.com>
2023-02-24 00:27:27 +0000

    number 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,15 @@
 
 /**
  * main - check the code.
- * _isupper - tests wether a character is upper or lowercase
+ * _isupper - tests whether a character is upper or lowercase and return 1 or 0
+ * respectively
  * Return: Always 0.
  */
 
 int _isupper(int c)
 {
-	int isupper(int c) {
-		return (c >= 'A' && c <= 'Z');
-	}
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
