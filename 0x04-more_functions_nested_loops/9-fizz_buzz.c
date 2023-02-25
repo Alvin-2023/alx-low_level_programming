7bb6fefc6db272d5097c1435b3557e0fbd908c14
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-24 20:06:32 -0800

    number 10

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,110 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+File Edit Options Buffers Tools C Help
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+  int i;
+
+for ( i = 0; i <= 100; i++ )
+{
+  if ( i % 3 == 0 &&  i % 5 == 0 )
+    {
+      printf("FizzBuzz");
+    }
+  else if (i % 3 = 0)
+    {
+      printf("Fizz");
+    }
+  else if ("i % 5 = 0)
+    {
+      printf("Buzz");
+    }
+  else
+    {
+      continue;
+    }
+
+printf("i");
+putchar(' ');
+
+}
+
+return (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-UUU:**--F1  9-fizz_buzz.c   All L8    Git-main  (C/*l Abbrev) ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Mark set
