6d74fade7777163e66989ea8e62239ef7675c08f
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-28 10:45:20 -0800

    number 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,8 @@
 /**
  * Main - entry point of our program
  * reset_to_98 - prints the re-evauluated int pointer
+ * @n - is our variable pointer
+ * @p - holds the value we update n with
  * Return - terminates main function
 */
 #include <stdio.h>
@@ -11,6 +13,5 @@ void reset_to_98(int *n)
 	int p;
 
 	p = 98;
-	*n = p;
-  
+	*n = p; 
 }
