1d2e157b0024b6d24185c8d2b36c8ed9bc59f241
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-28 10:53:52 -0800

    number 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,11 @@
  * reset_to_98 - prints the re-evauluated int pointer
  * @n - is our variable pointer
  * @p - holds the value we update n with
+ * @param - <parameter1> replaced the assigned value of p
  * Return - terminates main function
 */
 #include <stdio.h>
 #include "main.h"
-
 void reset_to_98(int *n)
 {
 	int p;
