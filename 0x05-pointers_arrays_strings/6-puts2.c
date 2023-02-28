66bdc1d6e8cd7033fa06c45080455df48fd4502b
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-28 12:44:41 -0800

    number 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+/**
+ * puts2 - prints every other character of a string, starting
+ *                     with the first character
+ *
+ * @str: pointer to the string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i += 2;
+	}
+
+	putchar('\n');
+}
+
