d4202efc10f4adee780b5fb76f5d46f6c684a887
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-28 10:07:23 -0800

    0x05 - pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/**
+ * Main - entry point of our program
+ * Return - terminates main function
+*/
+#include <stdio.h>
+#include "main.h"
+
+void reset_to_98(int *n)
+{
+  int p;
+  int *n;
+
+  *n = &p;
+  p = 98;
+  
+}
