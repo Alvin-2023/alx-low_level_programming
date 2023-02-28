d777f5b70cc935e38dd291b9bd24c76b13ea383f
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-28 11:44:36 -0800

    number 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_string - Prints a string followed by a new line to stdout
+ * @str: A pointer to the string to be printed
+ *
+ * Return: void
+ */
+void print_string(char *str)
+{
+    if (str == NULL)
+        return;
+
+    while (*str)
+        putchar(*str++);
+
+    putchar('\n');
+}
+
