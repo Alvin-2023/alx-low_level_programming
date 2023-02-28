dd2054e4deab131fa689ac30bd35f1bd159aee32
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-28 12:18:38 -0800

    number 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * rev_string - Reverses a string using pointers
+ * @s: A pointer to the string to be reversed
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	if (s == NULL)
+		return;
+
+	char *end = s;
+	char tmp;
+
+	while (*end !='\0')
+	    end++;
+
+	end--;
+
+	while (s < end)
+	{
+		tmp = *s;
+		*s++ = *end;
+		*end-- = tmp;
+	}
+}
+
