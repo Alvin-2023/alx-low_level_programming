2f16bb024e19e83de65a651a6d1eb0b92d5be747
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-02-28 11:27:46 -0800

    number 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/**
+ * string_length - Returns the length of a string.
+ *
+ * @str: A pointer to the string to get the length of.
+ *
+ * Return: The length of the string.
+ */
+int string_length(char *str)
+{
+        const char *ptr = str; /* Pointer to the beginning of the string */
+
+        while (*ptr != '\0') /* Loop until the null terminator is found */
+        {
+                ptr++; /* Increment the pointer */
+        }
+
+        return (ptr - str); /* Return the length of the string */
+}
+
