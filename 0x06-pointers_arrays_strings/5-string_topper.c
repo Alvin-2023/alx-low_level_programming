9cd7c0956830384bcf9fd6cadba74ed881d80fc9
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-03-01 08:11:19 -0800

    number 5

---
+++ 0x06-pointers_arrays_strings/5-string_topper.c
@@ -0,0 +1,21 @@
+/**
+ * string_toupper - convert all lowercase letters in a string to uppercase
+ * @s: pointer to the string
+ *
+ * Return: pointer to the converted string
+ */
+#include "main.h"
+#include <stdio.h>
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= ('a' - 'A');
+	}
+
+	return s;
+}
