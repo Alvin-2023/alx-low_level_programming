4521c78815c4026dd39060b07095f4c5a8374e5d
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-03-01 07:24:11 -0800

    number 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+/**
+ * _strncat - concatenates two strings.
+ *
+ * @dest: destination string.
+ * @src: source string.
+ * @n: maximum number of bytes to use from src.
+ *
+ * Return: a pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+
+	/* Find the end of the destination string */
+	while (*p != '\0')
+	{
+		p++;
+	}
+
+	/* Copy at most n bytes from the source string */
+	int i = 0;
+	while (i < n && *src != '\0')
+	{
+		*p++ = *src++;
+		i++;
+	}
+
+	/* Null-terminate the destination string */
+	*p = '\0';
+
+	return (dest);
+}
