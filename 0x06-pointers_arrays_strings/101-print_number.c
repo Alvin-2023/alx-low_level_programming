c6cd2a793bad8586d7007159e30f865fcb9df0d8
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-03-02 18:47:48 -0800

    number 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,23 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_number - prints an integer
+ * print_number - prints an integer followed by a newline
  * @n: integer to print
  *
  * Return: void
  */
 void print_number(int n)
 {
-	for (; n < 0;)
+	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n = -n;
-		_putchar('\n');
 	}
 
-	for (; n >= 0;)
-	{
-		_putchar(n);
-		_putchar('\n');
-	}
+	if (n / 10)
+		print_number(n / 10);
+
+	putchar(n % 10 + '0');
 }
