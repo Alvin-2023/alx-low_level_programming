965c27a09058e1d98190dacb422256f75cd733d2
Alvin Tayedzerwa <alvintayedzerwa@gmail.com>
2023-03-02 18:34:53 -0800

    number 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,14 +9,16 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	for (; n < 0;)
 	{
 		_putchar('-');
 		n = -n;
+		_putchar('\n');
 	}
 
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar(n % 10 + '0');
+	for (; n >= 0;)
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
 }
